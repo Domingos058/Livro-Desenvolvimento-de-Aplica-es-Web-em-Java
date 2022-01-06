d876ba4779d4179f24c50b7d762e270852d2b160
David Buzatto <davidbuzatto@gmail.com>
2022-01-06 00:19:36 -0300

    capítulo 10

--- projetos/capitulo10/VendaProdutos/src/main/java/vendaprodutos/VendaProdutos/VendaProdutosApplication.java
+++ projetos/capitulo10/VendaProdutos/src/main/java/vendaprodutos/VendaProdutos/VendaProdutosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VendaProdutosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VendaProdutosApplication.class, args);
-	}
+    public static void main( String[] args ) {
+        SpringApplication.run( VendaProdutosApplication.class, args );
+    }
 
 }
