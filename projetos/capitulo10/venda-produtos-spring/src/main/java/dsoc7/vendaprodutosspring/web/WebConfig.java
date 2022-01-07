54ef8d1ede09b93795c8dc70bad6b44971de56af
David Buzatto <davidbuzatto@gmail.com>
2022-01-06 23:56:47 -0300

    cap 10

---
+++ projetos/capitulo10/venda-produtos-spring/src/main/java/dsoc7/vendaprodutosspring/web/WebConfig.java
@@ -0,0 +1,24 @@
+package dsoc7.vendaprodutosspring.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Classe de configuração de views.
+ *
+ * @author Prof. Dr. David Buzatto
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addViewControllers( ViewControllerRegistry registry ) {
+        
+        // addViewController( caminho )
+        // setViewName( arquivo de template )
+        registry.addViewController( "/index" ).setViewName( "index" );
+        
+    }
+    
+}
